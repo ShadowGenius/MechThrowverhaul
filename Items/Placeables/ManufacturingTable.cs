c3df7e3ff9aef1c51d4b4ad8950d177335312fd6
William Lay <wlay2005@gmail.com>
2021-07-03 18:06:33 -0700

    Moved Manufacturing Table to Placeables

---
+++ Items/Placeables/ManufacturingTable.cs
@@ -0,0 +1,46 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MechThrowverhaul.Items.Placeables
+{
+    public class ManufacturingTable : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Allows you to restock throwing weapons.");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 48;
+            item.height = 41;
+            item.maxStack = 99;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.value = Item.buyPrice(0, 0, 50, 0);
+            item.createTile = ModContent.TileType<Tiles.ManufacturingTable>();
+        }
+
+        //public override void AddRecipes()
+        //{
+        //    ModRecipe recipe = new ModRecipe(mod);
+        //    recipe.AddIngredient(ItemID.IronBar, 10);
+        //    recipe.AddIngredient(ItemID.Wood, 15);
+        //    recipe.AddTile(TileID.Anvils);
+        //    recipe.SetResult(this);
+        //    recipe.AddRecipe();
+
+        //    ModRecipe recipe2 = new ModRecipe(mod);
+        //    recipe2.AddIngredient(ItemID.LeadBar, 10);
+        //    recipe2.AddIngredient(ItemID.Wood, 15);
+        //    recipe.AddTile(TileID.Anvils);
+        //    recipe2.SetResult(this);
+        //    recipe2.AddRecipe();
+        //}
+    }
+}
